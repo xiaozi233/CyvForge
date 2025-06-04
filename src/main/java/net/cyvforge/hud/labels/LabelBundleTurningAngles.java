311032d1a3d7a25cd84f3f8365074e7d6bf65669
Impulse188 <danielbnieto@outlook.com>
2025-06-04 20:53:43 +0400

    Update LabelBundleTurningAngles.java
    Added turn labels for ticks 3 through 12

--- src/main/java/net/cyvforge/hud/labels/LabelBundleTurningAngles.java
+++ src/main/java/net/cyvforge/hud/labels/LabelBundleTurningAngles.java
@@ -81,6 +81,346 @@ public class LabelBundleTurningAngles extends LabelBundle {
         });
 
         this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelThirdTurn";}
+            public String getDisplayName() {return "3: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(200,112);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String tt = df.format(ParkourTickListener.formatYaw(ParkourTickListener.tf - ParkourTickListener.sf));
+
+                drawString("3: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(tt, pos.getAbsoluteX() + 1 + font.getStringWidth("3: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("3: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("3: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+         this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelFourthTurn";}
+            public String getDisplayName() {return "4: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(200,103);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String fot = df.format(ParkourTickListener.formatYaw(ParkourTickListener.fof - ParkourTickListener.tf));
+
+                drawString("4: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(fot, pos.getAbsoluteX() + 1 + font.getStringWidth("4: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("4: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("4: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+ this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelFithTurn";}
+            public String getDisplayName() {return "5: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(200,94);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String fit = df.format(ParkourTickListener.formatYaw(ParkourTickListener.fif - ParkourTickListener.fof));
+
+                drawString("5: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(fit, pos.getAbsoluteX() + 1 + font.getStringWidth("5: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("5: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("5: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+         this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelSixthTurn";}
+            public String getDisplayName() {return "6: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(0, 112);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String sit = df.format(ParkourTickListener.formatYaw(ParkourTickListener.sif - ParkourTickListener.fif));
+
+                drawString("6: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(sit, pos.getAbsoluteX() + 1 + font.getStringWidth("6: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("6: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("6: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+ this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelSeventhTurn";}
+            public String getDisplayName() {return "7: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(200,85);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String sevt = df.format(ParkourTickListener.formatYaw(ParkourTickListener.sevf - ParkourTickListener.sif));
+
+                drawString("7: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(sevt, pos.getAbsoluteX() + 1 + font.getStringWidth("7: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("7: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("7: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+ this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelEigthTurn";}
+            public String getDisplayName() {return "8: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(200,76);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String eit = df.format(ParkourTickListener.formatYaw(ParkourTickListener.eif - ParkourTickListener.sevf));
+
+                drawString("8: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(eit, pos.getAbsoluteX() + 1 + font.getStringWidth("8: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("8: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("8: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+ this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelNinthTurn";}
+            public String getDisplayName() {return "9: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(200,67);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String nt = df.format(ParkourTickListener.formatYaw(ParkourTickListener.nf - ParkourTickListener.eif));
+
+                drawString("9: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(nt, pos.getAbsoluteX() + 1 + font.getStringWidth("9: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("9: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("9: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+ this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelTenthTurn";}
+            public String getDisplayName() {return "10: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(200,58);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String tet = df.format(ParkourTickListener.formatYaw(ParkourTickListener.tef - ParkourTickListener.nf));
+
+                drawString("10: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(tet, pos.getAbsoluteX() + 1 + font.getStringWidth("10: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("10: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("10: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+         this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelEleventhTurn";}
+            public String getDisplayName() {return "11: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(200, 49);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String elt = df.format(ParkourTickListener.formatYaw(ParkourTickListener.elf - ParkourTickListener.tef));
+
+                drawString("11: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(elt, pos.getAbsoluteX() + 1 + font.getStringWidth("11: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("11: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("11: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+ this.labels.add(new DraggableHUDElement() {
+            public String getName() {return "labelTwelthTurn";}
+            public String getDisplayName() {return "12: ";}
+            public int getWidth() {return getLabelWidth(getDisplayName());}
+            public int getHeight() {return getLabelHeight();}
+            public ScreenPosition getDefaultPosition() {return new ScreenPosition(200, 40);}
+            public void render(ScreenPosition pos) {
+                if (!this.isVisible) return;
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                DecimalFormat df = CyvForge.df;
+                String twt = df.format(ParkourTickListener.formatYaw(ParkourTickListener.twf - ParkourTickListener.elf));
+
+                drawString("12: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(twt, pos.getAbsoluteX() + 1 + font.getStringWidth("12: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+            public void renderDummy(ScreenPosition pos) {
+                int d = CyvClientConfig.getInt("df",3);
+                long color1 = CyvClientColorHelper.color1.drawColor;
+                long color2 = CyvClientColorHelper.color2.drawColor;
+                FontRenderer font = mc.fontRendererObj;
+
+                String str = "0.";
+                for (int i=0; i<CyvClientConfig.getInt("df",3); i++) str += "0";
+
+                drawString("12: ", pos.getAbsoluteX() + 1, (int) (pos.getAbsoluteY() + 1), color1);
+                drawString(str, pos.getAbsoluteX() + 1 + font.getStringWidth("12: ")
+                        , (int) (pos.getAbsoluteY() + 1), color2);
+            }
+        });
+
+        this.labels.add(new DraggableHUDElement() {
             public String getName() {return "labelPreturn";}
             public String getDisplayName() {return "Preturn";}
             public int getWidth() {return getLabelWidth(getDisplayName());}
